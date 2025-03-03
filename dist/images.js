d4abf2e145347df428f8ba44722c57c4289005e5
LUCHER4321 <lucher4321@gmail.com>
2025-03-03 00:28:41 -0300

    Create images.js

---
+++ dist/images.js
@@ -0,0 +1,7 @@
+export const images = new Map([
+    ["Makise Kurisu", "https://images2.alphacoders.com/732/732856.jpg"],
+    ["Navier Trovi", "https://i.pinimg.com/736x/f3/ec/f9/f3ecf9321ce2b36c171997a8ac7c6594.jpg"],
+    ["Honda Tohru", "https://i.pinimg.com/474x/49/ba/24/49ba2479e05d6599533d2430b472b7ee.jpg"],
+    ["Violet Evergarden", "https://hips.hearstapps.com/hmg-prod/images/violet-evergarden-642aded1d04a2.jpeg"],
+    ["Marceline", "https://i.pinimg.com/736x/cf/35/65/cf3565593090d443ed33662cfb6b435b.jpg"],
+]);
